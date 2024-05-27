e7dd8ea4b364e9b2417db9eb09104408439ac38b
brian_li <brian.li@sgs.com>
2024-05-27 14:47:49 +0800

    init project architecture

---
+++ SGS.HN.Labeler.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SGS.HN.Labeler.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
