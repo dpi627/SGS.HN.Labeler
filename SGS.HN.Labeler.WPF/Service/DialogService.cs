da68b28a0d8acd7c088d5e3e8504b9f69d4ed727
brian_li <brian.li@sgs.com>
2024-06-25 11:39:38 +0800

    修改 dialog

--- SGS.HN.Labeler.WPF/Service/DialogService.cs
+++ SGS.HN.Labeler.WPF/Service/DialogService.cs
@@ -1,12 +1,22 @@
 ﻿
 using MaterialDesignThemes.Wpf;
 using SGS.HN.Labeler.WPF.Control;
-using SGS.HN.Labeler.WPF.Model;
+using SGS.HN.Labeler.WPF.ViewModel;
 
 namespace SGS.HN.Labeler.WPF.Service;
 
 public class DialogService : IDialogService
 {
+    public void ShowMessage(string message, string buttonText = "OK")
+    {
+        var view = new DialogView
+        {
+            DataContext = new DialogViewModel(message, buttonText)
+        };
+
+        DialogHost.Show(view, "RootDialog");
+    }
+
     public async Task ShowMessageAsync(string message, string buttonText)
     {
         var view = new DialogView
