1d3bc85e2ab604903850b5bd8bf314024b539227
brian_li <brian.li@sgs.com>
2024-06-25 13:06:43 +0800

    完成 Excel 設定檔維護

--- SGS.HN.Labeler.Service/Interface/IExcelConfigService.cs
+++ SGS.HN.Labeler.Service/Interface/IExcelConfigService.cs
@@ -5,6 +5,7 @@
         public IEnumerable<ExcelConfigResultModel> GetList(string directoryPath);
         public ResultModel Import(ExcelConfigImportInfo info);
         public ResultModel Import(string sourcePath, string targetPath);
+        public ResultModel Delete(string filePath);
         public IEnumerable<PrintInfoResultModel> Load(string filePath);
     }
 }
